98d8f781027c8e54f9c9a5fee7f962b57858be78
Andreas Øverland <andreasoverland@gmail.com>
2017-02-07 20:56:00 +0100

    reduced

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 'use strict';
 
-
 let cards = [];
-for (let a = 0; a < 3; a++) {
-    for (let b = 0; b < 3; b++) {
-        for (let c = 0; c < 3; c++) {
-            for (let d = 0; d < 3; d++) {
-                cards.push( (a << 6) + (b << 4) + (c << 2) + d );
-            }
-        }
-    }
+for (let t=0;t<81;t++){
+    cards.push( (((t/27)%3)<<6) + (((t/9)%3)<<4) + (((t/3)%3)<<2) + (t%3) );
 }
 
 function cardToString( card ){
@@ -20,16 +13,9 @@ function cardToString( card ){
 function validate( c1, c2, c3 ){
 
     let result = true;
-
     for( let p=0;p<4 && result;p++){
-
-        const p1 = ( c1 >> p*2 ) & 0x3;
-        const p2 = ( c2 >> p*2 ) & 0x3;
-        const p3 = ( c3 >> p*2 ) & 0x3;
-
-        result &= checkProperty( p1, p2, p3 );
+        result &= checkProperty( ( c1 >> p*2 ) & 0x3, ( c2 >> p*2 ) & 0x3, ( c3 >> p*2 ) & 3 );
     }
-
     return result;
 
 }
